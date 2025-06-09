2efde3be62a5808af4b65a5bf280858d87fe6c7f
ivan meyer <meyerivanoliver@gmail.com>
2025-06-09 14:17:01 -0300

    default config zellij

--- modules/programs/zellij.nix
+++ modules/programs/zellij.nix
@@ -4,17 +4,5 @@
   programs.zellij = {
     enable = true;
     enableBashIntegration = true;
-    settings = {
-      keybinds = {
-        normal = {
-          "Alt-." = "NewPaneRight";
-          "Alt-," = "NewPaneRight";
-          "Alt-h" = "MoveFocusLeft";
-          "Alt-l" = "MoveFocusRight";
-          "Alt-k" = "MoveFocusUp";
-          "Alt-j" = "MoveFocusDown";
-        };
-      };
-    };
   };
 }
