b49c0e1bac042aca5c63938f06543b7b5803691a
ivan meyer <meyerivanoliver@gmail.com>
2025-06-10 13:20:44 -0300

    latest changes

--- hosts/maelstrom/configuration.nix
+++ hosts/maelstrom/configuration.nix
@@ -176,6 +176,10 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+  networking.firewall = {
+    enable = true;
+    allowPing = true;
+  };
 
   # This value determines the NixOS release from which the default # settings for stateful data, like file locations and database versions # on your system were taken. It‘s perfectly fine and recommended to leave # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
