242142b63fbbb1598e6ed3aca148c14ed5c2225e
ivan meyer <meyerivanoliver@gmail.com>
2025-08-28 18:55:04 -0300

    agregado thunderbird

--- modules/common/home_common.nix
+++ modules/common/home_common.nix
@@ -93,14 +93,14 @@ in
     };
   };
 
-  programs.gitui = {
+  programs.thunderbird = {
     enable = true;
-    keyConfig = ''
-      move_left: Some(( code: Char('h'), modifiers: "")),
-      move_right: Some(( code: Char('l'), modifiers: "")),
-      move_up: Some(( code: Char('k'), modifiers: "")),
-      move_down: Some(( code: Char('j'), modifiers: "")),
-    '';
+    profiles = {
+      ivan = {
+        isDefault = true;
+        search.default = "ddg";
+      };
+    };
   };
 
   programs.zoxide.enable = true;
