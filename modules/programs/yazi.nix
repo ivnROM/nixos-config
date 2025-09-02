333d99d35faf04d53059659b430f4ed14e001332
ivan meyer <meyerivanoliver@gmail.com>
2025-09-01 21:01:22 -0300

    yazi keymaps

--- modules/programs/yazi.nix
+++ modules/programs/yazi.nix
@@ -7,5 +7,36 @@
     enable = true;
     enableBashIntegration = true;
     shellWrapperName = "y";
+    keymap = {
+      mgr = {
+        prepend_keymap = [
+          {
+            on   = "!";
+            for  = "unix";
+            run  = "shell \"$SHELL\" --block";
+            desc = "Open $SHELL here";
+          }
+        ];
+      };
+    };
+    settings = {
+      opener = {
+        read = [
+          {
+            run = "okular \"$@\"";
+            orphan = true;
+            for = "unix";
+          }
+        ];
+      };
+      open = {
+        append_rules = [
+          {
+            name = "*.pdf";
+            use = "read";
+          }
+        ];
+      };
+    };
   };
 }
