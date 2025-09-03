15113dc39975ca8492ba6c997562a464371abed2
ivan meyer <meyerivanoliver@gmail.com>
2025-09-03 16:58:42 -0300

    plymouth en boot menu

--- modules/common/global_common.nix
+++ modules/common/global_common.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, input, ...}:
+{ config, pkgs, input, ...}: let
+  logo = pkgs.fetchurl {
+    url = "https://nixos.org/logo/nixos-hires.png";
+    sha256 = "1ivzgd7iz0i06y36p8m5w48fd8pjqwxhdaavc0pxs7w1g7mcy5si";
+  };
+in
 {
 
   imports = [
@@ -54,9 +59,15 @@
     ly.enable = true;
   };
 
+  boot.plymouth = {
+    enable = true;
+    logo = logo;
+  };
+
   stylix.targets = {
     font-packages.enable = true;
     console.enable = true;
+    plymouth.enable = true;
   };
 
   # deshabilitar en caso de dejar de usar dolphin
--- modules/programs/starship/starship.nix
+++ modules/programs/starship/starship.nix
@@ -3,19 +3,12 @@
 {
   programs.starship = {
     enable = true;
-    # settings = pkgs.lib.importTOML ./config.toml;
     settings = {
       add_newline = false;
       format = lib.concatStrings [
-        # "$all"
-        # "$nix_shell$nodejs$lua$golang$rust$php$git_branch$git_commit$git_state$git_status\n$username$hostname$directory"
         "$username$hostname$directory"
       ];
       scan_timeout = 10;
-      # character = {
-      #   success_symbol = "➜";
-      #   error_symbol = "➜";
-      # };
     };
   };
 }
